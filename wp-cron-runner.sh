f93950dee26b9f1ab916f51adc0d11ff0bfa8632
firefallpro <info@firefallpro.com>
2016-11-14 12:03:24 -0500

    Checkout test.

--- wp-cron-runner.sh
+++ wp-cron-runner.sh
@@ -6,7 +6,7 @@
 # Handles cron jobs that run wp-cron.php for various WordPress installs that share the same server
 # by adding a random delay to reduce possible performance costs that would be incurred by running
 # scripts concurrently. The script also appends a timestamp to prevent caching if any caching service is used.
-
+#
 # @version 0.0.1, 2016-11-11
 # @author Scott Park <scott@firefallpro.com>, Aric Ng <aric@firefallpro.com>
 # @link http://www.firefallpro.com/
