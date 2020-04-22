23ddedbeb349068cd9d931041372ebfe483cb95d
Scott Park <info@firefall.io>
2020-04-22 17:19:32 -0400

    Update wp-cron-runner.sh
    Syntax fixes based on shellcheck.net
    
    Added print_usage function.

--- wp-cron-runner.sh
+++ wp-cron-runner.sh
@@ -14,59 +14,68 @@
 verbose="-s -S"
 resolve=""
 offset=0
-url=$1
+url=$1 # Get first argument
+
+print_usage() {
+	echo 'Usage
+/usr/local/bin/wp-cron-runner.sh http://www.example.com/wp-cron.php
+./wp-cron-runner.sh -v -h <host>:<port>:<ip> http://example.com/wp-cron.php'
+}
 
 # Look for v in options; stop script if other options are found
-while getopts ":vh:" opt; do 
+while getopts ":vh:" opt; do
 	case $opt in
-		v) 
+		v)
 			# v found, shift options
 			printf "\nwp-cron-runner: Verbose mode\n\n"
-			verbose="-v" 
-			offset=$(($offset + 1))
+			verbose="-v"
+			offset=$((offset + 1))
 			;;
 		h)
 			# h found, set --resolve and its argument for curl
 			# argument should be similar to example.com:80:10.0.1.1
 			resolve="--resolve $OPTARG"
-			
+
 			offset=$((offset + 2))
 			;;
-		*) 
-			printf "\nwp-cron-runner: Unexpected flag found: -$OPTARG: Script Terminated\n\n"
+		*)
+			printf "\nwp-cron-runner: Unexpected flag found: -%s: Script Terminated\n\n" "$OPTARG"
+			print_usage
 			exit 1
 			;;
 	esac
+
+	print_usage
 done
 
 # Look for absolute url after offset
 for ((c = 0; c < offset; c++))
 do
-	shift $(($OPTIND-1))
+	shift $((OPTIND - 1))
 done
 
 url=$1
 
 # Check if URL is empty
 if [ -z "$url" ]; then
-	printf "\nwp-cron-runner.sh: Expecting URL: Empty argument supplied: Script Terminated\n\n" 
+	printf "\nwp-cron-runner.sh: Expecting URL: Empty argument supplied: Script Terminated\n\n"
 	exit 1
 fi
 
 if [[ $verbose == "-v" ]]; then
-	printf "\nwp-cron-runner.sh: Attempting to run cURL command on $url"
-	
+	printf "\nwp-cron-runner.sh: Attempting to run cURL command on %s" "$url"
+
 	if [ -n "$resolve" ]; then
-		printf " with host $resolve"
+		printf " with host %s" "$resolve"
 	fi
 fi
 
 # This runs wp-cron.php with up to 60 seconds of entropy to prevent overlap with other sites
-delay=$(($RANDOM%60));
+delay=$((RANDOM % 60));
 
 if [ "$verbose" == "-v" ]; then
-	printf "\nwp-cron-runner.sh: Sleeping for $delay secs \n\n"
+	printf "\nwp-cron-runner.sh: Sleeping for %s secs \n\n" "$delay"
 fi
 
 /bin/sleep $delay;
-/usr/bin/curl --ipv4 $verbose $resolve $url?`date +\%s`;
+/usr/bin/curl --ipv4 $verbose "$resolve" "$url?$(date + %S)";
